680da29056bcbd2e87c1a7062fdbda69d223771f
Tink Zhang <ZhangYunfengZJU@gmail.com>
2023-08-10 18:33:46 +0800

    hide api key

--- nyt-fetcher.py
+++ nyt-fetcher.py
@@ -1,6 +1,7 @@
 import firebase_admin
 import requests
 import json
+import sys
 from types import SimpleNamespace
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -14,8 +15,7 @@ db = firestore.client()
 
 # Fiction
 doc_ref = db.collection(u'Fictions')
-
-response = requests.get('https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=HZt6QgrKva8Vbj4BOGkxgIRkGmTz2e2H').text
+response = requests.get('https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key='+sys.argv[0]).text
 re = json.loads(response, object_hook=lambda d: SimpleNamespace(**d))
 print(re.results.published_date)
 books = re.results.books
@@ -47,7 +47,7 @@ for i in range(len(books)):
 # NonFiction
 doc_ref = db.collection(u'NonFictions')
 
-response = requests.get('https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=HZt6QgrKva8Vbj4BOGkxgIRkGmTz2e2H').text
+response = requests.get('https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key='+sys.argv[0]).text
 re = json.loads(response, object_hook=lambda d: SimpleNamespace(**d))
 print(re.results.published_date)
 books = re.results.books
